82578f46c88185daa38024a3cc1de3564c46b790
Gregg Wong <griekup@live.com>
2021-09-16 10:04:49 -0500

    Add project files.

---
+++ AwesomApp/AwesomApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using AwesomApp.ViewModels;
+
+using System.ComponentModel;
+
+using Xamarin.Forms;
+
+namespace AwesomApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
