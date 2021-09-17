b1902fea63656fc48f4c54a48232f97869484293
Gregg Wong <griekup@live.com>
2021-09-16 19:58:42 -0500

    Wire up view model

--- AwesomApp/AwesomApp/Views/FoodEquipmentPage.xaml.cs
+++ AwesomApp/AwesomApp/Views/FoodEquipmentPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using AwesomApp.ViewModels;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +15,13 @@ namespace AwesomApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FoodEquipmentPage : ContentPage
     {
+
         public FoodEquipmentPage()
         {
             InitializeComponent();
+            BindingContext = new FoodEquipmentViewModel();
         }
+
+     
     }
 }
