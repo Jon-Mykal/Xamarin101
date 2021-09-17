772c56aa173c020bb1768f02c12170d648e15c70
Gregg Wong <griekup@live.com>
2021-09-16 19:55:32 -0500

    Add ViewModel based on BindableObject

---
+++ AwesomApp/AwesomApp/ViewModels/FoodEquipmentViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace AwesomApp.ViewModels
+{
+    public class FoodEquipmentViewModel : BindableObject
+    {
+        public FoodEquipmentViewModel()
+        {
+            IncreaseCountCmd = new Command(IncreaseCount);
+        }
+        string countDisplay = "Click Me!";
+        int count = 0;
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                // If incoming value (value) is 
+                // the same as the current value (countDisplay)
+                // don't change
+                if (value == countDisplay)
+                {
+                    return;
+                }
+
+                countDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand IncreaseCountCmd { get; }
+
+        void IncreaseCount()
+        {
+            count++;
+            CountDisplay = $"You clicked {count} time(s)";
+        }
+    }
+}
