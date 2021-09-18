83d379ab6e6bcfeac150b8d2d7adf646284d4b88
Gregg Wong <griekup@live.com>
2021-09-17 22:38:20 -0500

    Create ViewModelBase

---
+++ AwesomApp/AwesomApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿
+using ReactiveUI;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace AwesomApp.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+        public ViewModelBase()
+        {
+           
+        }
+
+        public string Title { get; set; }
+
+        bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+    }
+}
