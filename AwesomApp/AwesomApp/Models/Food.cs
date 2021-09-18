4fb1104af4b9a1748e551439788323202f317fc9
Gregg Wong <griekup@live.com>
2021-09-17 22:37:40 -0500

    Add food model

---
+++ AwesomApp/AwesomApp/Models/Food.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomApp.Models
+{
+    public class Food
+    {
+        public Food()
+        {
+
+        }
+
+        public string Kitchen { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+    }
+}
