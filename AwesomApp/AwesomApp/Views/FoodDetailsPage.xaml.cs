493e58819de995322a1a6f542d8a8687664f597b
Gregg Wong <griekup@live.com>
2021-09-21 21:58:04 -0500

    Add UI and wire up view model for Food Details page

---
+++ AwesomApp/AwesomApp/Views/FoodDetailsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using AwesomApp.Services;
+using AwesomApp.ViewModels;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [QueryProperty(nameof(FoodId), nameof(FoodId))]
+    public partial class FoodDetailsPage : ContentPage
+    {
+
+        public int FoodId { get; set; }
+        public FoodDetailsPage()
+        {
+            InitializeComponent();
+            BindingContext = new FoodDetailsViewModel();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var foods = await FoodService.GetAllFood();
+            var food = foods.FirstOrDefault(f => f.Id == FoodId);
+
+            if (food != null)
+            {
+                ((FoodDetailsViewModel)BindingContext).Food = food;
+            }
+        }
+    }
+}
