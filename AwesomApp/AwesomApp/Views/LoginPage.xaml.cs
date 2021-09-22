8a9b3a9e6717962cc35a2d0b9d5eff677604371d
Gregg Wong <griekup@live.com>
2021-09-22 12:35:21 -0500

    Add Login Page

---
+++ AwesomApp/AwesomApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        const string key = "loggedIn";
+        public LoginPage()
+        {
+            InitializeComponent();
+          
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            if (Preferences.ContainsKey(key))
+            {
+                var loggedIn = Preferences.Get(key, false);
+                if (loggedIn)
+                {
+                    await Shell.Current.GoToAsync($"//{nameof(FoodEquipmentPage)}");
+                }
+            }
+        }
+
+        // TODO: Replace this with ViewModel
+        public async void OnLoginBtnClicked(object sender, EventArgs e)
+        {
+            Preferences.Set(key, true);
+            await Shell.Current.GoToAsync($"//{nameof(FoodEquipmentPage)}");
+        }
+    }
+}
