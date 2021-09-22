5f58c8d0f2cfcfc0addb9859cff7f052b94c4ade
Gregg Wong <griekup@live.com>
2021-09-21 21:51:05 -0500

    Add FoodDetailsViewModel

---
+++ AwesomApp/AwesomApp/ViewModels/FoodDetailsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AwesomApp.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace AwesomApp.ViewModels
+{
+    
+    public class FoodDetailsViewModel
+    {
+
+        public FoodDetailsViewModel()
+        {
+            DoneCmd = new AsyncCommand(GoBack);
+        }
+
+        public Food Food { get; set; }
+
+        public AsyncCommand DoneCmd { get; set; }
+
+        async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
