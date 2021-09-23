b6e01aa58e5369e098735e74ce1b137f3d4f77c0
Gregg Wong <griekup@live.com>
2021-09-23 10:13:35 -0500

    Add LoginViewModel for Login Page

---
+++ AwesomApp/AwesomApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using AwesomApp.Views;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AwesomApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+            LoginCmd = new AsyncCommand(Login);
+        }
+
+        public AsyncCommand LoginCmd { get; }
+
+
+        async Task Login()
+        {
+            Preferences.Set("loggedIn", true);
+            await Shell.Current.GoToAsync($"//{nameof(FoodEquipmentPage)}");
+        }
+    }
+}
