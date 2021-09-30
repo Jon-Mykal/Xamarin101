3987510a2c60abdfc2b597e35aa17fd18ce30d31
Gregg Wong <griekup@live.com>
2021-09-30 13:51:46 -0500

    Add Font exports

--- AwesomApp/AwesomApp/AssemblyInfo.cs
+++ AwesomApp/AwesomApp/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Font Awesome 6 Brands-Regular-400.otf", Alias = "FAB")]
+[assembly: ExportFont("Font Awesome 6 Free-Regular-400.otf", Alias = "FAR")]
+[assembly: ExportFont("Font Awesome 6 Free-Solid-900.otf", Alias = "FAS")]
+[assembly: ExportFont("OpenSans-VariableFont_wdth,wght.ttf", Alias = "OpenSans")]
