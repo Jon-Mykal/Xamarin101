e0fe283df2e029d55b712d1c789de8c7bba5eb6a
Gregg Wong <griekup@live.com>
2021-10-13 14:37:25 -0500

    Add interfaces for toasts

---
+++ AwesomApp/AwesomApp/Contracts/IMessageDialog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomApp.Contracts
+{
+    public interface IMessageDialog
+    {
+        void ShowMessage(string title, string message, string buttonText);
+    }
+}
