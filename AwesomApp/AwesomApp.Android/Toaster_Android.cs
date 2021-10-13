684f93fafc801097eb82942a2516042ea40be7ec
Gregg Wong <griekup@live.com>
2021-10-13 14:38:26 -0500

    Implement Android specific toast

---
+++ AwesomApp/AwesomApp.Android/Toaster_Android.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using AwesomApp.Contracts;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Essentials;
+
+namespace AwesomApp.Droid
+{
+    public class Toaster_Android : IToast
+    {
+        public void MakeToast(string message)
+        {
+            Toast.MakeText(Platform.AppContext, message, ToastLength.Long).Show();
+        }
+    }
+}
