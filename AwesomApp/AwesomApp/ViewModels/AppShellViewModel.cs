de22a55b6582165c37c911259d2a7bf729f67176
Gregg Wong <griekup@live.com>
2021-10-13 10:10:51 -0500

    Add AppShellViewModel

---
+++ AwesomApp/AwesomApp/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomApp.ViewModels
+{
+    public class AppShellViewModel : ViewModelBase
+    {
+        public AppShellViewModel()
+        {
+
+        }
+        private string _header,
+            _legalese,
+            _menuItem,
+            _listItem;
+
+        public string Header
+        {
+            get => _header;
+            set => SetProperty(ref _header, value);  
+        }
+
+        public string Legalese
+        {
+            get => _legalese;
+            set => SetProperty(ref _legalese, value);
+        }
+
+        public string MenuItem
+        {
+            get => _menuItem;
+            set => SetProperty(ref _menuItem, value);  
+        }
+
+        public string ListItem
+        {
+            get => _listItem;
+            set => SetProperty(ref _listItem, value);
+        }
+    }
+}
