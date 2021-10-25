074b8ebf8416b2d89357c8a47e84fb8a4f02dc95
Gregg Wong <griekup@live.com>
2021-10-25 11:29:37 -0500

    Add Username and Password

--- AwesomApp/AwesomApp/ViewModels/LoginViewModel.cs
+++ AwesomApp/AwesomApp/ViewModels/LoginViewModel.cs
@@ -11,20 +11,66 @@ using Xamarin.Forms;
 
 namespace AwesomApp.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : ViewModelBase
     {
         public LoginViewModel()
         {
-            LoginCmd = new AsyncCommand(Login);
+            LoginCmd = new AsyncCommand(Login, IsValid);
+            UserName = "";
+            Password = "";
+        }
+
+        private string _userName,
+                       _password;
+
+        public string UserName
+        {
+            get => _userName;
+            set  
+            {
+                var isSet = SetProperty(ref _userName, value);
+                if (isSet)
+                {
+                    LoginCmd.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => _password;   
+            set
+            {
+                var isSet = SetProperty(ref _password, value);
+                if (isSet)
+                {
+                    LoginCmd.RaiseCanExecuteChanged();
+                }
+            }
         }
 
         public AsyncCommand LoginCmd { get; }
 
-
+        bool IsValid() => !string.IsNullOrWhiteSpace(_userName) && !string.IsNullOrWhiteSpace(_password);
         async Task Login()
         {
-            Preferences.Set("loggedIn", true);
-            await Shell.Current.GoToAsync($"//{nameof(FoodEquipmentPage)}");
+            if (!IsValid())
+            {
+                return;
+            }
+
+            if (UserName.Equals("admin") && Password.Equals("password123"))
+            {
+                Preferences.Set("loggedIn", true);
+               
+                
+                await Shell.Current.GoToAsync($"//{nameof(FoodEquipmentPage)}");
+            
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Sign In Status", "Username or password is incorrect. Please try again", "Ok");
+            }
         }
     }
 }
