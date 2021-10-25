d6b402b7e54b2ec8b29290aaea4016399dddb422
Gregg Wong <griekup@live.com>
2021-10-25 11:26:50 -0500

    Add Custom iOS renderer for entry

---
+++ AwesomApp/AwesomApp.iOS/Renderers/AwesomeApp_EntryRenderer_iOS.cs
@@ -0,0 +1,31 @@
+﻿using AwesomApp.Controls;
+using AwesomApp.iOS.Renderers;
+
+using Foundation;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(AwesomApp_Entry), typeof(AwesomeApp_EntryRenderer_iOS))]
+namespace AwesomApp.iOS.Renderers
+{
+    public class AwesomeApp_EntryRenderer_iOS : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.TintColor = UIColor.FromRGB(29, 93, 236);
+            }
+        }
+    }
+}
